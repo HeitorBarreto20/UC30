2f6c6a1465a307523ce6ac04ebdfa6503dfd0659
Heitor Barreto de Araújo <heit.barreto.09@gmail.com>
2026-03-04 08:31:40 -0300

    Add files via upload

---
+++ UC - Algoritimo (04.03.26)/listas.py
@@ -0,0 +1,25 @@
+nome1 = "Ana"
+nome2 = "Bruno"
+nome3 = "Carlos"
+
+nomes = ["Alice", "João", "Maria"]
+print (nomes)
+
+dados = ["Gabriel", 0, 1.60, True]
+print (dados)
+print (type(dados))
+print (type(dados[0]))
+print (type(dados[1]))
+print (type(dados[2]))
+print (type(dados[3]))
+
+alunos = ["Adriano", "Bárbara", "Camila"]
+print ("Primeiro", alunos[0])
+print ("Segundo", alunos[1])
+print ("Terceiro", len(alunos))
+print ("Quarto", alunos [len(alunos) - 1])
+
+nomes = ["Ana", "Bruno", "Caio"]
+print ("Original", nomes)
+nomes.append ("Daniel")
+print ("Atualizado: ", nomes)
