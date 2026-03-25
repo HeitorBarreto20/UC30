be2ad960d64188ebbdfe62f4ce88ed6445684cc1
Heitor Barreto de Araújo <heit.barreto.09@gmail.com>
2026-03-25 15:53:48 -0300

    Add files via upload

---
+++ UC - Algoritimo (25.03.26(Contraturno Parte 01))/Atividade_de_argumentos.py
@@ -0,0 +1,17 @@
+def calcularnotas (*notas):
+
+    nota01 = 7
+    nota02 = 8
+    nota03 = 9
+
+    soma = nota01 + nota02 + nota03
+    média = (nota01 + nota02 + nota03) / 3
+    maior = nota03
+    menor = nota01
+
+    print (f"A soma das notas é {soma}!")
+    print (f"A média das notas é {média}!")
+    print (f"A maior nota é {maior}!")
+    print (f"A menor nota é {menor}!")
+
+calcularnotas (7, 8 ,9)
